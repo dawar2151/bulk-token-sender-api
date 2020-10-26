31911c7eb054cbec54c7809d1afaf33d99be88a7
et-taousy <zouhirtaousy@gmail.com>
2020-10-26 11:37:42 +0100

    Add conf file

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ git clone https://github.com/et-taousy/bridge-api
 cd bridge-api
 yarn
 ```
+# Config Mongo
+open the file .env and add your mongodb uri.
 # Run
 ```sh
 npm run start
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+
 import { TransactionsModule } from './transactions/transactions.module';
 import { TokensModule } from './tokens/tokens.module';
 import { BridgesModule } from './bridges/bridges.module';
@@ -9,12 +12,14 @@ import { AccountsModule } from './accounts/accounts.module';
 import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/pets-api'),
-  TransactionsModule  ,
-  TokensModule,
-  AccountsModule,
-  BridgesModule,
-  WalletsModule
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URI),
+    TransactionsModule  ,
+    TokensModule,
+    AccountsModule,
+    BridgesModule,
+    WalletsModule
 ],
   controllers: [AppController],
   providers: [AppService],
